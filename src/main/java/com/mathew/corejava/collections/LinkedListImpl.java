5fa6376cadc17cc0a791b76d3c3453514fbeef63
mathew78george <mathew.george@clarivate.com>
2018-08-22 14:34:22 +0530

    LinkedList
    LinkedList

--- src/main/java/com/mathew/corejava/collections/LinkedListImpl.java
+++ src/main/java/com/mathew/corejava/collections/LinkedListImpl.java
@@ -1,6 +1,5 @@
 package com.mathew.corejava.collections;
 
-
 public class LinkedListImpl<E> {
 
 	private LinkedListItem<E> head;
@@ -30,20 +29,18 @@ public class LinkedListImpl<E> {
 	}
 
 	public void remove(E data) {
+		int index = 0;
 		for (LinkedListItem<E> x = head; x != null; x = x.getNext()) {
-			LinkedListItem<E> previous = x;
-			LinkedListItem<E> next = x.getNext();
+			index++;
 			if (data.equals(x.getItem())) {
-				previous.setItem(null);
-				previous.setNext(next);
-//				if (next != null) {
-//					previous.setNext(next);
-//				} else {
-//					previous.setItem(null);
-//					previous.setNext(null);
-//				}
+				break;
 			}
 		}
+		LinkedListItem<E> current = head;
+		for (int ii = 0; ii < index - 1; ii++) {
+			current = current.getNext();
+		}
+		current.setNext(current.getNext().getNext());
 	}
 
 	public static void main(String[] args) {
@@ -56,7 +53,7 @@ public class LinkedListImpl<E> {
 		ll.add("TWO");
 		ll.add("5");
 		ll.add("6");
-		
+
 	}
 
 }
